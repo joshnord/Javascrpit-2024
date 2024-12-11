6eee0530146fd7e66bec2e2fd42eb6b8c38d156d
joshnord <jnordstrom25@riverdale.k12.or.us>
2024-12-11 10:46:13 -0800

    3 Kilograms = -4 Pounds

---
+++ Javascript 1-2/script2.js
@@ -0,0 +1,57 @@
+
+var metValue = 0;
+var impValue = 0;
+// var numberValue = 0;
+// window.onload = function(){
+//     //happens after the page has loaded 
+//     numberValue = document.querySelector("#valueBox");
+// }
+
+
+
+
+
+function convert(){
+//we should only do the functon "convert" when button "Convert" is pressed
+
+    var interim = document.querySelector("#valueBox");
+    var cVal = interim.value;
+    console.log ("cVal = " + cVal);
+
+    var convers = document.querySelector("#conversion");
+    var conv = convers.value;
+    console.log ("conv = " + conv);
+
+
+
+
+if (conv == "celsius"){
+    console.log ("option = " + conv);
+    
+    metValue = parseInt(valueBox.value) + 8;
+    impValue = parseInt(valueBox.value) - 8;
+
+    console.log ("new metValue = " + metValue);
+    console.log ("new impValue = " + impValue);
+
+    var hTwo = document.querySelector("h2");
+    hTwo.innerText = valueBox.value + " Degrees Fahrenheit = " + metValue + " Degrees Celsius" +"\n"+ valueBox.value + " Degrees Celsius = " + impValue + " Degrees Fahrenheit";
+}
+
+if (conv == "kilo"){
+    console.log ("option = " + conv);
+
+    metValue = parseInt(valueBox.value) + 7;
+    impValue = parseInt(valueBox.value) - 7;
+
+    console.log ("new metValue = " + metValue);
+    console.log ("new impValue = " + impValue);
+
+    var hTwo = document.querySelector("h2");
+    hTwo.innerText = valueBox.value + " Pounds = " + metValue + " Kilograms" + "\n" + valueBox.value + "Kilograms = " + impValue + " Pounds";
+}
+
+if (conv == "liter"){
+   
+}}
+
