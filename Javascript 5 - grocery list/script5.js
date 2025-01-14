86abf6b610158950272ca8cd67db8dc341338966
joshnord <jnordstrom25@riverdale.k12.or.us>
2025-01-14 11:12:56 -0800

    hiding works

--- Javascript 5 - grocery list/script5.js
+++ Javascript 5 - grocery list/script5.js
@@ -2,75 +2,41 @@
 
 var arrayOne = [];  
 
+var shouldHide = false;
 
-// function doSomething(el){
-//     alert (el.html);
-//     } 
 
 function doOtherThing(){
 
-    // doOtherThingg();
-var n = document.querySelectorAll (".done");
-console.log(n);
-// All
+    shouldHide = !shouldHide;
 
-// .classList
-for (var i = 0; i<n.length; i++){
+    var n = document.querySelectorAll (".done");
+    console.log(n);
 
-console.log (n[i].innerText);
-n[i].classList.toggle("hide");
-
+    for (var q = 0; q < n.length; q++){
+        if(shouldHide ){
+            n[q].classList.add("hide");
+        }
+        else{
+            n[q].classList.remove("hide");
+        }
+    }
 }
-
-}
-
-//boolean hidden = false;
-//function hide{
-    //hidden = !hidden;
-//}
-//if(hidden)
+    
 
 function doSomething(el){
     el.classList.toggle("done");
-    
-// function doOtherThingg(){
-//     el.classList.toggle("hide");
-
-// }
 }
 
-    //    li onclick = doSomething(this);
+    
 function addItem(){
     
-    //get value of textbox (v)
-    //add value to array
-    // // var li document.createElement ("li");
-    // li.innerText = v; 
-    // li.classList.append("NewItem");
-    // var myList = document.querySelector("#list");
-    // myList.appendChild(li);
     var newItemBox = document.querySelector("#newItem");
     var newItem = newItemBox.value;
-        // alert("something stupid = " + newItem)
-        
     
-    //createl list ite
     var li = document.createElement("li");
     li.innerText = newItem;
 
-    li.setAttribute("onclick", "doSomething(this)")
-
-// classlist.add
-// claslist. remove
-
-//.clsslist 
-// onclick can be done to any element
-// .toggle
-
-
-
-
-    // li.classList.add ("done");
+    li.setAttribute("onclick", "doSomething(this)");
 
     arrayOne.push (newItem);
     //add item to list
@@ -78,11 +44,8 @@ function addItem(){
     myList.appendChild(li);
 
 
-
     for(var i = 0; i < arrayOne.length; i++){
     
     console.log (arrayOne[i]);
     }
-
 }
-
