f29381764d85ef23339a88f842c5b2224659011e
joshnord <jnordstrom25@riverdale.k12.or.us>
2025-02-04 11:34:13 -0800

    i love the github coded message

---
+++ Javascript 7 - CLUE/game ideas.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+// potential games:
+
+
+// tic tac toe
+// minesweeper
+// clue????? - could be hype (score via least moves)]  
+// solitare
+// battleship
+// tetris
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// potential games:
+
+
+// tic tac toe
+// checkers
+// minesweeper
+// wordle
+// monopoly???
+// sorry?
+// clue????? - could be hype (score via least moves)
+// chess??    WR1 WK1 WB1 W+ WQ WB2 WK2 WR2 WP1 WP2 WP3...       
+// 2048
+// solitare
+// battleship
+// tetris
+
+
+
+//NOT
+// pac man
+// snake
+
+
